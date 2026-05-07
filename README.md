0374ace7f73d842d0a9040c173b9431ad336aa57
PAVANI AJMEERA <pavaniajmeera@PAVANIs-MacBook-Air.local>
2026-05-07 22:53:59 +1000

    addded nav bar

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# React + Vite && react_burger
+## react_burger
+1. Created a react vite burger app
+2. Clean up all data 
+3. Added cdns
+4. created HTML and CSS for burger website 
+5. created all components as assumed 
+6. git second committ 
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='logo'>
+        <div className='title'>Burger</div>
+        <div className='nav'>
+                <a href="">Home</a>
+                <a href="">Menu</a>
+                <a href="">About</a>
+                <a href="">Conact</a>
+        </div>
+    </div>
   )
 }
