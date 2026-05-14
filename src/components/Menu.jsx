d741cf9039a2c3b69cbb40a3b926c6ce7555019b
bhaskarajmeera <bhaskar.ajmeera@zohomail.com.au>
2026-05-14 22:39:32 +1000

    menu added

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,63 @@
-import React from 'react'
+import React from 'react';
+import img1 from "../assets/yardspecial.png";
 
-export const Menu = ({img,}) => {
+
+export const Menu = () => {
+  const items = [
+    {
+      name: "Yard Special Burger",
+      price: "$15",
+      img: img1,
+      desc: "Wagyu Beef, American Cheese, Pickled Gherkins, Fresh Lettuce, Slaw ,Jalapenos, Yard Signature Sauce & Spicy Peri Sauce."
+    },
+    {
+      name: "Backyard Clasic",
+      price: "$15",
+      img: img1,
+      desc: "Wagyu Beef, American Cheese, Red Onion, Pickled Gherkins, Fresh Lettuce, Tomotoes, Yard Signature Sauce & Trufle Sauce. "
+    },
+    {
+      name: "Spicy Fire Burger",
+      price: "$13.99",
+      img: img1,
+      desc: "Jalapeños, spicy mayo & pepper jack cheese."
+    },
+    {
+      name: "Chicken Crunch Burger",
+      price: "$11.99",
+      img: img1,
+      desc: "Crispy fried chicken, lettuce & creamy mayo."
+    },
+    {
+      name: "Loaded Fries",
+      price: "$15",
+      img: img1,
+      desc: "Cheese sauce, bacon bits & spring onions."
+    },
+    {
+      name: "Thick Milkshake",
+      price: "$8.90",
+      img: img1,
+      desc: "Vanilla, chocolate or strawberry."
+    }
+  ];
   
   return (
-    <div className='menu' style={{color:"blue"}}>
-    <img src="" alt='burger image'/>
-    <h2>Title</h2>
-    <p>wagyu Beef,American Cheese,Pickled Gherkins,Fresh Lettuce,Slaw,Jalapenos,Yard Signature Sauce and Spicy Peri Sauce</p>
+    <>
+    <section className="menu" id="menu">
+      <h2 className="menu-title">Our Menu</h2>
 
-
-    </div>
+      <div className="menu-grid">
+        {items.map((item, index) => (
+          <div className="menu-card" key={index}>
+            <img src={item.img} alt={item.name} className="menu-img" />
+            <h3>{item.name}</h3>
+            <p className="menu-desc">{item.desc}</p>
+            <p className="menu-price">{item.price}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+    </>
   )
 }
